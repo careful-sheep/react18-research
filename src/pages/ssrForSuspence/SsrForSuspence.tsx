9670c0f9462c27e71ee3bf3b141f84ac80e5f312
careful-sheep <1092979868@qq.com>
2022-06-10 20:30:28 +0800

    lottie动画

--- src/pages/ssrForSuspence/SsrForSuspence.tsx
+++ src/pages/ssrForSuspence/SsrForSuspence.tsx
@@ -1,5 +1,16 @@
-const SsrForSuspence: React.FC = () => {
-    return null;
-};
+import lottie from 'lottie-web';
+import { useEffect } from 'react';
 
+const SsrForSuspence: React.FC = () => {
+    useEffect(() => {
+        lottie.loadAnimation({
+            container: document.getElementById('lottie-animation') as HTMLDivElement,
+            renderer: 'svg',
+            loop: true,
+            path: 'https://gaeacdn.jiliguala.com/jlgl/pages/animation/finger02.json',
+            autoplay: true
+        });
+    }, []);
+    return <div id="lottie-animation" style={{ width: '300px', height: '300px' }}></div>;
+};
 export default SsrForSuspence;
